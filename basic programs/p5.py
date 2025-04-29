32ca888f2e6d45e58debbce0fc4b7dea672d4f2b
vikashm29 <vikasmahadev33@gmail.com>
2025-04-29 12:27:02 +0530

    p5 added

---
+++ basic programs/p5.py
@@ -0,0 +1,6 @@
+input_alpha = input('Enter an alphabet:')
+
+if input_alpha >= 'a'  and input_alpha <= 'z':
+print(input_alpha, 'is a small alphabet')
+else:
+print(f'{input_alpha}  is not a small alphabet')
