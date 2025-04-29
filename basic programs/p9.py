3d286fc7450f6bf09533d7bf8fb17785c0bf5622
vikashm29 <vikasmahadev33@gmail.com>
2025-04-29 21:38:02 +0530

    p9 p10 added

--- basic programs/p9.py
+++ basic programs/p9.py
@@ -1,4 +1,4 @@
-input_number = input('Enter the input number:')
+input_number = input('Enter the 2 numberss:')
 
-if input_number <= 'biggest of 2 number' :
-    print (input_number, 'is Biggest of 2 number')
+if input_number <= 'a < b and b < a':
+    print (input_number, 'is Biggest of 2 numbers')
