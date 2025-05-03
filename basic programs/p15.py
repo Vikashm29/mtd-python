a42ccaa1862747ade08a7ccd29552580b143cde4
vikashm29 <vikasmahadev33@gmail.com>
2025-05-03 09:49:37 +0530

    done

--- basic programs/p15.py
+++ basic programs/p15.py
@@ -2,6 +2,6 @@ number_of_line = int(input('Enter number of lines of the Square: '))
 
 for i in range(1 , number_of_line+1):
     if i == 1 or i == number_of_line:
-       print('* ' * number_of_line)
+     print('*' * number_of_line)
 else:
-    print('* ' + (' ' * ((number_of_line-2) * 2)) + '*')
+     print('*' + ' ' * (number_of_line-2) + '*')
